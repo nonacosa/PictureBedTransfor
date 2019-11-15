10b57b6d220c1daa99269b4176da26bb660ee190
庄文达 <pkwenda@163.com>
2019-11-16 02:26:21 +0800

    init

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+
+
+
+func upload(bed_type string){
+	switch bed_type {
+	case "minio":
+		minio()
+	}
+}
+
+func minio()  {
+
+}
+
+// target is directory path or file path
+func analysis_text(target string)  {
+
+}
